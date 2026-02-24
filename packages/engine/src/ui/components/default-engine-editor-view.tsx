1c4489291a2ab4f6be703c8113756d3b473b1197
Lleyton Morris <lleyton92@gmail.com>
2026-02-24 04:55:05 +0000

    feat: initial tab sidebar

--- packages/engine/src/ui/components/default-engine-editor-view.tsx
+++ packages/engine/src/ui/components/default-engine-editor-view.tsx
@@ -1,3 +1,5 @@
+import { Cube, TreeStructure } from "@phosphor-icons/react";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import { EngineUiContext, type EngineUiContextValue } from "../utilities/engine-context";
 import { EngineCanvas } from "./engine-canvas";
 import { EngineEditorLayout } from "./engine-editor-layout";
@@ -17,6 +19,9 @@ type DefaultEngineEditorViewProps = {
  *   COMPONENT START
  **********************************************************************************************************/
 export const DefaultEngineEditorView: React.FC<DefaultEngineEditorViewProps> = ({ engine }) => {
+  const inactiveIconColor = "#8c95aa";
+  const activeIconColor = "#7387ff";
+
   return (
     <EngineUiContext value={engine}>
       <PreviewMode>
@@ -30,10 +35,53 @@ export const DefaultEngineEditorView: React.FC<DefaultEngineEditorViewProps> = (
 
             <PreviewMode.Disabled>
               <EngineEditorLayout.LeftSidebar>
-                <EngineEditorLayout.PanelTitle>Worlds / Entities</EngineEditorLayout.PanelTitle>
-                <EngineEditorLayout.PanelContent className={styles.worldsEntitiesPanelContent}>
-                  <WorldsEntitiesPanel />
-                </EngineEditorLayout.PanelContent>
+                <TabGroup className={styles.leftSidebarTabsRoot}>
+                  <TabPanels className={styles.leftSidebarTabPanels}>
+                    <TabPanel className={styles.leftSidebarTabPanel}>
+                      <EngineEditorLayout.PanelTitle>Worlds / Entities</EngineEditorLayout.PanelTitle>
+                      <EngineEditorLayout.PanelContent className={styles.worldsEntitiesPanelContent}>
+                        <WorldsEntitiesPanel />
+                      </EngineEditorLayout.PanelContent>
+                    </TabPanel>
+
+                    <TabPanel className={styles.leftSidebarTabPanel}>
+                      <EngineEditorLayout.PanelTitle>Components</EngineEditorLayout.PanelTitle>
+                      <EngineEditorLayout.PanelContent>Components placeholder</EngineEditorLayout.PanelContent>
+                    </TabPanel>
+                  </TabPanels>
+
+                  <TabList className={styles.leftSidebarTabList}>
+                    <Tab className={styles.leftSidebarTabButton}>
+                      {({ selected }) => (
+                        <div className={styles.leftSidebarTabIconSlot}>
+                          <div
+                            className={styles.leftSidebarTabIconLayer}
+                            style={{
+                              transform: selected ? "translate(-50%, -50%) scale(1.12)" : "translate(-50%, -50%) scale(1)",
+                            }}
+                          >
+                            <TreeStructure size={18} color={selected ? activeIconColor : inactiveIconColor} weight="regular" />
+                          </div>
+                        </div>
+                      )}
+                    </Tab>
+
+                    <Tab className={styles.leftSidebarTabButton}>
+                      {({ selected }) => (
+                        <div className={styles.leftSidebarTabIconSlot}>
+                          <div
+                            className={styles.leftSidebarTabIconLayer}
+                            style={{
+                              transform: selected ? "translate(-50%, -50%) scale(1.12)" : "translate(-50%, -50%) scale(1)",
+                            }}
+                          >
+                            <Cube size={18} color={selected ? activeIconColor : inactiveIconColor} weight="regular" />
+                          </div>
+                        </div>
+                      )}
+                    </Tab>
+                  </TabList>
+                </TabGroup>
               </EngineEditorLayout.LeftSidebar>
             </PreviewMode.Disabled>
 
@@ -42,15 +90,15 @@ export const DefaultEngineEditorView: React.FC<DefaultEngineEditorViewProps> = (
             </EngineEditorLayout.Center>
 
             <PreviewMode.Disabled>
-              <EngineEditorLayout.RightSidebar>
+              {/* <EngineEditorLayout.RightSidebar>
                 <EngineEditorLayout.PanelTitle>Components</EngineEditorLayout.PanelTitle>
                 <EngineEditorLayout.PanelContent>Right sidebar placeholder</EngineEditorLayout.PanelContent>
-              </EngineEditorLayout.RightSidebar>
+              </EngineEditorLayout.RightSidebar> */}
 
-              <EngineEditorLayout.BottomBar>
+              {/* <EngineEditorLayout.BottomBar>
                 <EngineEditorLayout.PanelTitle>Output / Timeline</EngineEditorLayout.PanelTitle>
                 <EngineEditorLayout.PanelContent>Bottom bar placeholder</EngineEditorLayout.PanelContent>
-              </EngineEditorLayout.BottomBar>
+              </EngineEditorLayout.BottomBar> */}
             </PreviewMode.Disabled>
           </EngineEditorLayout.Root>
         </div>
