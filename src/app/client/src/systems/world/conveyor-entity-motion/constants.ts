c83f4842a4ebda0dc2d0bde1f001297fc715a860
Lleyton Morris <lleyton92@gmail.com>
2026-03-07 09:36:35 +0000

    fix: items move at conveyor speeds

--- src/app/client/src/systems/world/conveyor-entity-motion/constants.ts
+++ src/app/client/src/systems/world/conveyor-entity-motion/constants.ts
@@ -4,6 +4,6 @@ import { Vec2 } from "@engine";
 export const CONVEYOR_SIDES: readonly ConveyorSide[] = ["left", "right"];
 export const CONVEYOR_SLOT_INDICES_ASC: readonly ConveyorSlotIndex[] = [0, 1, 2, 3];
 export const CONVEYOR_SLOT_INDICES_DESC: readonly ConveyorSlotIndex[] = [3, 2, 1, 0];
-export const DEMO_SLOT_ADVANCE_DURATION_MS = 5_000;
+export const DEMO_SLOT_ADVANCE_DURATION_MS = 1_075;
 export const DEMO_SLOT_PROGRESS_PER_MILLISECOND = 1 / DEMO_SLOT_ADVANCE_DURATION_MS;
 export const SHARED_SLOT_POSITION = new Vec2();
